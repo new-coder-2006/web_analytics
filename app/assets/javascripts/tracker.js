86227f56f91c0fe525cae568650af8426a6e0f45
Zach Lloyd <zachlloyd@Zachs-MacBook-Pro.local>
2024-05-30 10:30:26 -0400

    Tracks visits and durations

--- app/assets/javascripts/tracker.js
+++ app/assets/javascripts/tracker.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var visitDuration = 0;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/track", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-    var params = "page_url=" + encodeURIComponent(window.location.href) + "&duration=" + visitDuration;
-    xhr.send(params);
+    var startTime = new Date().getTime();
+    window.onbeforeunload = sendRequest;
+    function sendRequest() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/track", true);
+        var visitDuration = new Date().getTime() - startTime;
+        var params = "page_url=" + encodeURIComponent(window.location.href) + "&duration=" + visitDuration;
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(params);
+    }
   });
